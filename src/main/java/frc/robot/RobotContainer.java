0030df4aaac87c60bc76647e5f898b1f2af0997c
y0shi <y0shi@github.com>
2024-02-04 14:16:32 -0500

    WIP on charaterization

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -108,7 +108,7 @@ public class RobotContainer implements Logged {
     new JoystickButton(m_driverController, XboxController.Button.kRightBumper.value)
     .whileTrue(new RunIntake(m_robotIntake, outtakePower));
     new JoystickButton(m_driverController, XboxController.Button.kY.value)
-        .whileTrue(m_robotDrive.driveSysIdTestBuilder(4, 3));
+        .whileTrue(m_robotDrive.driveSysIdTestBuilder(4, 1.75));
     new JoystickButton(m_driverController, XboxController.Button.kB.value)
         .whileTrue(m_robotDrive.turnSysIdTestBuilder(7, 3));
   }
