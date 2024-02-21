ac81dbdbac06fdcf5cd4d76123cff43b8d9c3d06
Orcasphynx <richardwilliamcrouch@gmail.com>
2024-02-20 20:22:16 -0500

    Controllable Acceleration

--- src/main/java/frc/robot/subsystems/DriveSubsystem.java
+++ src/main/java/frc/robot/subsystems/DriveSubsystem.java
@@ -107,9 +107,6 @@ public class DriveSubsystem extends SubsystemBase implements Logged{
   @Log.NT
 @Log.File
   private PathPlannerPath logged_path; 
-
-  private SlewRateLimiter m_magLimiter = new SlewRateLimiter(DriveConstants.kMagnitudeSlewRate);
-  private SlewRateLimiter m_rotLimiter = new SlewRateLimiter(DriveConstants.kRotationalSlewRate);
   private double m_prevTime = WPIUtilJNI.now() * 1e-6;
 
   // Odometry class for tracking robot pose
@@ -317,8 +314,11 @@ public class DriveSubsystem extends SubsystemBase implements Logged{
    *                      field.
    * @param rateLimit     Whether to enable rate limiting for smoother control.
    */
-  public void drive(double xSpeed, double ySpeed, double rot, boolean fieldRelative, boolean rateLimit) {
+  public void drive(double xSpeed, double ySpeed, double rot, boolean fieldRelative, boolean rateLimit, SlewRateLimiter magLimiter, SlewRateLimiter rotLimiter, double kDirectionSlewRate) {
     
+    SlewRateLimiter m_magLimiter = magLimiter;
+    SlewRateLimiter m_rotLimiter = rotLimiter;
+    double m_kDirectionSlewRate = kDirectionSlewRate;
     double xSpeedCommanded;
     double ySpeedCommanded;
 
@@ -334,7 +334,7 @@ public class DriveSubsystem extends SubsystemBase implements Logged{
       // Calculate the direction slew rate based on an estimate of the lateral acceleration
       double directionSlewRate;
       if (m_currentTranslationMag != 0.0) {
-        directionSlewRate = Math.abs(DriveConstants.kDirectionSlewRate / m_currentTranslationMag);
+        directionSlewRate = Math.abs(m_kDirectionSlewRate / m_currentTranslationMag);
       } else {
         directionSlewRate = 500.0; //some high number that means the slew rate is effectively instantaneous
       }
