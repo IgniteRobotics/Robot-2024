b4c7bcd4785e28115921e836419038a5d262d9ee
y0shi <y0shi@github.com>
2024-03-20 23:22:25 -0400

    Using new target finder

--- src/main/java/frc/robot/subsystems/drive/PhotonCameraWrapper.java
+++ src/main/java/frc/robot/subsystems/drive/PhotonCameraWrapper.java
@@ -8,15 +8,49 @@ import org.photonvision.EstimatedRobotPose;
 import org.photonvision.PhotonCamera;
 import org.photonvision.PhotonPoseEstimator;
 import org.photonvision.PhotonPoseEstimator.PoseStrategy;
+import org.photonvision.targeting.PhotonPipelineResult;
 import org.photonvision.targeting.PhotonTrackedTarget;
 
 import edu.wpi.first.apriltag.AprilTagFieldLayout;
 import edu.wpi.first.apriltag.AprilTagFields;
 import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Transform3d;
 import edu.wpi.first.wpilibj.Timer;
 import frc.robot.Constants.CameraConstants;
 
 public class PhotonCameraWrapper {
+
+    public class TargetInfo{
+        private double yaw;
+        private double distance;
+
+        public TargetInfo(double distance, double yaw){
+            this.distance = distance;
+            this.yaw = yaw;
+        }
+
+        public double getYaw() {
+            return yaw;
+        }
+
+        public void setYaw(double yaw) {
+            this.yaw = yaw;
+        }
+
+        public double getDistance() {
+            return distance;
+        }
+
+        public void setDistance(double distance) {
+            this.distance = distance;
+        }
+    }
+
+
+
+
+
+
     public PhotonCamera photonCameraFrontLeft;
     public PhotonPoseEstimator photonPoseEstimatorFrontLeft;
     public PhotonCamera photonCameraFrontRight;
@@ -85,17 +119,17 @@ public class PhotonCameraWrapper {
         return getEstimatedGlobalPose(prevEstimatedRobotPose, Side.FRONT_LEFT);
     }
 
-    public Optional<Double> getYawToTarget(int id){
+    public Optional<TargetInfo> seekTarget(int id){
         
         //we're using a camera that was already locked on
         if (null != m_targetCam){  
             var result = m_targetCam.getLatestResult();
-            for (var target : result.getTargets()){
-                if (id == target.getFiducialId()){
-                    m_targetTimer.restart();
-                    return Optional.of(Double.valueOf(target.getYaw()));
-                }
+            Optional<PhotonTrackedTarget> target = lookForTarget(result, id);
+            if (target.isPresent()){
+                m_targetTimer.restart();
+                return Optional.of(new TargetInfo(getDistanceFromTransform3d(target.get().getBestCameraToTarget()), target.get().getYaw()));
             }
+
             //we didn't find it with the locked camera.
             //if the timer has expired, go back to all cams.
             if(m_targetTimer.hasElapsed(m_lockTimeSec)){
@@ -105,12 +139,11 @@ public class PhotonCameraWrapper {
         } else { //no pre locked camera.  loop through them all.
             for (PhotonCamera cam : allCameras){
                 var newResult = cam.getLatestResult();
-                for (var target : newResult.getTargets()){
-                    if (id == target.getFiducialId()){
-                        m_targetCam = cam;
-                        m_targetTimer.restart();
-                        return Optional.of(Double.valueOf(target.getYaw()));
-                    }
+                Optional<PhotonTrackedTarget> target = lookForTarget(newResult, id);
+                if (target.isPresent()){
+                    m_targetCam = cam;
+                    m_targetTimer.restart();
+                    return Optional.of(new TargetInfo(getDistanceFromTransform3d(target.get().getBestCameraToTarget()), target.get().getYaw()));
                 }
             }
         }
@@ -120,6 +153,22 @@ public class PhotonCameraWrapper {
         m_targetTimer.stop();
         return Optional.empty();
 
+    }
+
+    private Optional<PhotonTrackedTarget> lookForTarget(PhotonPipelineResult result, int targetId){
+        for (var target : result.getTargets()){
+                if (targetId == target.getFiducialId()){
+                    return Optional.of(target) ;
+                }
+            }
+        return Optional.empty();
+    }
+
+    private double getDistanceFromTransform3d(Transform3d t){
+        return Math.sqrt(
+                Math.pow(t.getX(), 2) + 
+                Math.pow(t.getY(), 2)
+        );
     }
 
     public void unlockTargeting(){
