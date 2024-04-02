9b2841b978759b47474ef0fe23a5eee6db929bed
Orcasphynx <richardwilliamcrouch@gmail.com>
2024-04-01 20:59:02 -0400

    Simulator is weird, but I fixed a potential issue

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -296,7 +296,7 @@ private static class Operator {
     autonChooser.addOption("Gollum", AutoBuilder.buildAuto("Gollum"));
     autonChooser.addOption("Subside 3 piece auton (center pieces)", AutoBuilder.buildAuto("Subside 3 piece auton (center pieces)"));
     autonChooser.addOption("4RingClose", AutoBuilder.buildAuto("4RingClose"));
-    autonChooser.addOption("FourRingImproved", AutoBuilder.buildAuto("FourRingImproved"));
+    autonChooser.addOption("FourRingImproved", AutoBuilder.buildAuto("4RingImproved"));
     SmartDashboard.putData("Autonomous", autonChooser);
 
 
